3efd34be253da7f0e9dcc53ff9e5a9b203bc958f
lucianocolin <lucianocolin99@gmail.com>
2025-05-22 17:29:59 -0300

    Add register user errors handling

---
+++ src/constants/auth/register-user-messages.ts
@@ -0,0 +1,2 @@
+export const REGISTER_USER_ERROR = "There was an error registering the user";
+export const REGISTER_USER_SUCCESS = "User registered successfully!";
---
+++ src/constants/auth/register-user-schema-errors.ts
@@ -0,0 +1,14 @@
+export const FULL_NAME_REQUIRED = "Full name is required";
+export const FULL_NAME_MIN = "Full name must be at least 2 characters long";
+export const FULL_NAME_MAX = "Full name must be at most 50 characters long";
+
+export const EMAIL_REQUIRED = "Email is required";
+export const EMAIL_FORMAT = "Email is invalid";
+
+export const PASSWORD_REQUIRED = "Password is required";
+export const PASSWORD_FORMAT =
+  "Password should be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character";
+
+export const PHONE_NUMBER_REQUIRED = "Phone number is required";
+export const PHONE_NUMBER_MIN =
+  "Phone number must be at least 2 characters long";
