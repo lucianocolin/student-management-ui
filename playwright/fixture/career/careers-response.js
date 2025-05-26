95d5a6476cc4d37895b93fd3b38a6e4992165f13
lucianocolin <lucianocolin99@gmail.com>
2025-05-26 19:02:22 -0300

    Add create student tests

---
+++ playwright/fixture/career/careers-response.js
@@ -0,0 +1,14 @@
+export const careersResponse = [
+  {
+    id: "c325d451-258f-480d-b0d9-7a09ff1556de",
+    name: "Computer Science",
+  },
+  {
+    id: "c325d451-258f-480d-b0d9-7a09ff1556df",
+    name: "Math",
+  },
+  {
+    id: "c325d451-258f-480d-b0d9-7a09ff1556dg",
+    name: "Physics",
+  },
+];
---
+++ playwright/fixture/user/my-user-without-student-response.js
@@ -0,0 +1,7 @@
+export const myUserWithoutStudentResponse = {
+  id: "d3031e17-7ee4-41a6-83fc-88bfb530e65f",
+  fullName: "Test User",
+  email: "testuser@test.com",
+  phoneNumber: "1234567890",
+  roles: ["user", "admin"],
+};
