4af16450ba6b5cfa3c20ddffa8bec9d1523ce65f
lucianocolin <lucianocolin99@gmail.com>
2025-06-02 22:59:42 -0300

    Add query to get all students

--- src/services/student/useStudent.ts
+++ src/services/student/useStudent.ts
@@ -2,6 +2,23 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { api } from "../../configs/axios";
 import type { IStudentResponse } from "../../interfaces/student/IStudentResponse";
 import type { ICreateStudentProps } from "../../interfaces/student/ICreateStudentProps";
+import { useDebounce } from "use-debounce";
+
+export const useGetStudents = (search?: string) => {
+  const [searchDebounce] = useDebounce(search, 300);
+  const queryParams = searchDebounce ? `?search=${searchDebounce}` : "";
+
+  return useQuery({
+    queryKey: ["students", searchDebounce],
+    queryFn: async () => {
+      const { data: response } = await api.get<IStudentResponse[]>(
+        `/student${queryParams}`
+      );
+
+      return response;
+    },
+  });
+};
 
 export const useGetStudent = (id: string) => {
   return useQuery({
