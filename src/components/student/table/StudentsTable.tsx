3dea32b5559cd94c6ef5427010647dd87ab19c09
lucianocolin <lucianocolin99@gmail.com>
2025-06-04 00:38:46 -0300

    Modify data test id from students table

--- src/components/student/table/StudentsTable.tsx
+++ src/components/student/table/StudentsTable.tsx
@@ -32,7 +32,7 @@ const StudentsTable = ({
     <div>
       <table
         className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-        data-testid="student-table"
+        data-testid="students-table"
       >
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           {table.getHeaderGroups().map((headerGroup) => (
