42eef77b46fe07d471047f00ed8c91d6e978a3b7
lucianocolin <lucianocolin99@gmail.com>
2025-06-05 16:53:28 -0300

    Add success and error messages

--- src/constants/student/student-messages.ts
+++ src/constants/student/student-messages.ts
@@ -1,2 +1,4 @@
 export const CREATE_STUDENT_SUCCESS = "Student created successfully!";
 export const CREATE_STUDENT_ERROR = "There was an error creating the student";
+export const DELETE_STUDENT_SUCCESS = "Student deleted successfully!";
+export const DELETE_STUDENT_ERROR = "There was an error deleting the student";
