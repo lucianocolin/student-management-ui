e3e407cc6fb4d410f407c8dcbc19896b59301521
lucianocolin <lucianocolin99@gmail.com>
2025-06-10 20:09:53 -0300

    Add assign grades tests

---
+++ playwright/fixture/enrollment/enrollmentUpdatedResponse.js
@@ -0,0 +1,26 @@
+export const enrollmentUpdatedResponse = {
+  id: "c325d451-258f-480d-b0d9-7a09ff1556dd",
+  studentId: "c325d451-258f-480d-b0d9-7a09ff1556de",
+  subjectId: "c325d451-258f-480d-b0d9-7a09ff1556df",
+  grade: 7.0,
+  enrolledAt: "2023-05-25T01:51:11.037Z",
+  approved: false,
+  student: {
+    id: "c325d451-258f-480d-b0d9-7a09ff1556de",
+    fullName: "Test User",
+    email: "testuser@test.com",
+    collegeId: 100,
+    careerId: "c325d451-258f-480d-b0d9-7a09ff1556de",
+    createdAt: "2025-05-25T01:51:11.037Z",
+    updatedAt: "2025-05-25T01:51:11.037Z",
+    deletedAt: null,
+    career: {
+      id: "c325d451-258f-480d-b0d9-7a09ff1556de",
+      name: "Computer Science",
+    },
+  },
+  subject: {
+    id: "c325d451-258f-480d-b0d9-7a09ff1556df",
+    name: "Programming",
+  },
+};
