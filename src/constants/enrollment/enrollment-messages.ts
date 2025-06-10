6ac61320cb3a814ea6fae5ff6cd0d8de8ea8f2ce
lucianocolin <lucianocolin99@gmail.com>
2025-06-10 20:09:15 -0300

    Add assign grade messages

--- src/constants/enrollment/enrollment-messages.ts
+++ src/constants/enrollment/enrollment-messages.ts
@@ -1,3 +1,5 @@
 export const CREATE_ENROLLMENT_ERROR =
   "There was an error creating the enrollment";
 export const CREATE_ENROLLMENT_SUCCESS = "Enrollment created successfully!";
+export const ASSIGN_GRADE_ERROR = "There was an error assigning the grade";
+export const ASSIGN_GRADE_SUCCESS = "Grade assigned successfully!";
