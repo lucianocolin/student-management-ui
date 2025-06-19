ed58c9695724ed0c8377d921f114dd7c148b145b
lucianocolin <lucianocolin99@gmail.com>
2025-06-18 23:42:54 -0300

    Add registration requests messages

---
+++ src/constants/auth/registration-request-messages.ts
@@ -0,0 +1,4 @@
+export const APPROVE_REGISTRATION_REQUEST_ERROR =
+  "There was an error approving the registration request";
+export const APPROVE_REGISTRATION_REQUEST_SUCCESS =
+  "Registration request approved successfully!";
